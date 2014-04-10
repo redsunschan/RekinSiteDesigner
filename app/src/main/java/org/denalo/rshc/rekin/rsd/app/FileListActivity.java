202ffd14c27fe9be477848eb296820b2d7f71075
Redsuns S.H.Chan <redsunschan@gmail.com>
2014-04-10 19:07:39 +0800

    UPDATE

--- app/src/main/java/org/denalo/rshc/rekin/rsd/app/FileListActivity.java
+++ app/src/main/java/org/denalo/rshc/rekin/rsd/app/FileListActivity.java
@@ -1,27 +1,29 @@
+/*******************************************************************************
+ * Copyright (c) Denalo Entertainment Organization
+ ******************************************************************************/
+
 package org.denalo.rshc.rekin.rsd.app;
 import android.app.*;
 import android.os.*;
 import android.view.*;
 import android.widget.*;
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
-public class FileListActivity extends Activity
+public class FileListActivity extends ListActivity
 	{
-		
-		public ListView view;
-		public ArrayAdapter<String> adapter;
-		public String[] list;
-		
+
+        private List<String> items = new ArrayList<String>();
+        private String parentPath = "/";
+        private ArrayAdapter<String> adapter;
+
 		@Override
 		protected void onCreate ( Bundle savedInstanceState )
 			{
-				// TODO: Implement this method
-				view = new ListView ( this );
-				list = new File ( Rekin.rootPath ).list ( );
-				adapter = new ArrayAdapter<String> ( getApplicationContext ( ) , android.R.layout.simple_list_item_1 , list );
-				view.setAdapter ( adapter );
-				setContentView ( view );
-				super.onCreate ( savedInstanceState );
+                super.onCreate ( savedInstanceState );
+                setContentView ( R.layout.activity_file );
+                getFile ( new File( "/sdcard/" ).listFiles ( ) );
 			}
 
 		@Override
@@ -30,5 +32,25 @@ public class FileListActivity extends Activity
 				getMenuInflater ( ).inflate ( R.menu.file_list , menu );
 				return super.onCreateOptionsMenu ( menu );
 			}
-		
+
+        private void getFile ( File[] files ) {
+            items.clear ( );
+            items.add ( "../ ( Parent Path )" );//Add Parent Path Item
+
+            for ( File file : files ) {
+                if ( file.getParentFile ( ) != null && file.getParentFile ( ).getParent ( ) != null ) {
+                    parentPath = file.getParentFile().getParent();
+                }
+
+                if ( file.isDirectory ( ) ) {
+                    items.add ( file.getPath() );
+                } else {
+                    items.add ( file.getName ( ) );
+                }
+            }
+
+            adapter = new ArrayAdapter<String>( this , android.R.layout.simple_list_item_1 , items );
+            this.setListAdapter ( adapter );
+        }
+
 }
